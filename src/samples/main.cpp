47205cd827acb8dec9b0654d334b7359d7e3afa4
Agga <adrian@nukez.de>
2015-06-24 00:53:57 +0200

    additional project setup

--- src/samples/main.cpp
+++ src/samples/main.cpp
@@ -4,23 +4,26 @@
 #include <tiki/config.h>
 #include <tiki/version.h>
 
-int WinMain( char* args, int argc )
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+int main(char* args, int argc)
 {
-	std::cout << "Test" << std::endl;
-	std::cin.get();
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		return 1;
+
+	SDL_Window* sdlWindow = SDL_CreateWindow("Tiki Hunt",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800,
+		600,
+		SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE );
+
+	if (sdlWindow == nullptr)
+		return 1;
+
+	SDL_Delay(5000);
+	SDL_Quit();
 	return 0;
 }
 
-int main()
-{
-	std::cout << "Test" << std::endl;
-	std::cin.get();
-	return 0;
-}
-
-
-
-//int WinMain(HINSTANCE *, HINSTANCE *, char *, int)
-//{
-//	return 0;
-//}
--- windows.cmd
+++ windows.cmd
@@ -1,8 +1,25 @@
-@echo off
+@echo off &setlocal
+
+if defined VS120COMNTOOLS (
+	set generator=Visual Studio 12 2013
+	set buildtool=VS120COMNTOOLS
+	goto:createProjectFiles
+)
+
+if defined VS110COMNTOOLS (
+	set generator=Visual Studio 11 2012
+	set buildtool=VS110COMNTOOLS
+	goto:createProjectFiles
+)
+
+echo no visual studio found
+goto:end
+
+:createProjectFiles
 
 if not exist .\proj_win mkdir .\proj_win
 pushd proj_win
-cmake -G "Visual Studio 12 2013 Win64" ../src
+cmake -G "%generator% Win64" ../src
 
 for %%A in (%*) do (
 	for %%B in (Debug, Release, Master) do (
@@ -12,12 +29,15 @@ for %%A in (%*) do (
 	)
 )
 
+popd
 goto:end
 
 :build
 echo Build %1
-CALL "%VS120COMNTOOLS%..\IDE\devenv.exe" tiki.sln /Build %1
+SETLOCAL ENABLEDELAYEDEXPANSION
+CALL "!%buildtool%!..\IDE\devenv.exe" tiki.sln /Build %1
+SETLOCAL DISABLEDELAYEDEXPANSION
 goto:eof
 
 :end
-popd
+
